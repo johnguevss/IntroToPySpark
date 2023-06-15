66103be6d3894ba97618609bab7478169fbfd43b
johnguevss <118093526+johnguevss@users.noreply.github.com>
2023-06-16 00:27:10 +0800

    added files for exercise

---
+++ config.py
@@ -0,0 +1,12 @@
+import os
+
+access_key = os.environ.get("TWITTER_API_KEY") 
+access_secret = os.environ.get("TWITTER_API_SECRET")
+consumer_key = os.environ.get("TWITTER_ACCESS_TOKEN")
+consumer_secret = os.environ.get("TWITTER_ACCESS_TOKEN_SECRET")
+
+
+# consumer_key = "YOUR_CONSUMER_KEY"
+# consumer_secret = "YOUR_CONSUMER_SECRET"
+# access_token = "YOUR_ACCESS_TOKEN"
+# access_token_secret = "YOUR_ACCESS_TOKEN_SECRET"
