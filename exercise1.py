13cc04c819c57b9ea580a95c100722e3bedc6305
johnguevss <118093526+johnguevss@users.noreply.github.com>
2023-06-16 00:24:58 +0800

    Update exercise1.py

--- exercise1.py
+++ exercise1.py
@@ -1,29 +1,75 @@
 import tweepy
-
-# Twitter API credentials
-consumer_key = "YOUR_CONSUMER_KEY"
-consumer_secret = "YOUR_CONSUMER_SECRET"
-access_token = "YOUR_ACCESS_TOKEN"
-access_token_secret = "YOUR_ACCESS_TOKEN_SECRET"
+import json
+from datetime import datetime
+from config import *
+import spark
+from pyspark.sql import SparkSession
 
 # Authenticate with Twitter API
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
+auth= tweepy.OAuth1UserHandler(access_key, access_secret)
+auth.set_access_token(consumer_key, consumer_secret)
 
 # Create API object
 api = tweepy.API(auth)
 
 # Hashtag to search
-hashtag = "#example"
+hashtag = "#NBAPlayoffs"
 
 # Number of tweets to retrieve
-num_tweets = 10
+num_tweets = 20
 
-# Retrieve tweets
-tweets = tweepy.Cursor(api.search, q=hashtag, tweet_mode='extended').items(num_tweets)
+# File path to save the tweets
+output_file = "tweets.txt"
 
-# Process and print each tweet
-for tweet in tweets:
-    print("Username:", tweet.user.screen_name)
-    print("Tweet:", tweet.full_text)
-    print("--------")
+# Open the file in write mode
+with open(output_file, "w", encoding="utf-8") as file:
+    # Retrieve tweets
+    tweets = tweepy.Cursor(api.search, q=hashtag, tweet_mode='extended').items(num_tweets)
+
+    # Process and write each tweet to the file
+    for tweet in tweets:
+        file.write("Username: {}\n".format(tweet.user.screen_name))
+        file.write("Tweet: {}\n".format(tweet.full_text))
+        file.write("--------\n")
+
+# Print a message once all tweets are written to the file
+print("Tweets saved to {}.".format(output_file))
+
+
+# initiate a local spark session
+
+# remove words in this list
+stop_words = ['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', 'your', 'yours', 'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she', 'her', 'hers', 'herself', 'it', 'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves', 'what', 'which', 'who', 'whom', 'this', 'that', 'these', 'those', 'am', 'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does', 'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into', 'through', 'during', 'before', 'after', 'above', 'below', 'to', 'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further', 'then', 'once', 'here', 'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more', 'most', 'other', 'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so', 'than', 'too', 'very', 'can', 'will', 'just', 'don', 'should', 'now']
+
+# Create a baseRDD from the file path
+baseRDD = spark.textFile('tweets.txt')
+
+# Split the lines of baseRDD into words
+splitRDD = baseRDD.flatMap(lambda x: x.split())
+
+# Count the total number of words
+print("Total number of words in splitRDD:", splitRDD.count())
+
+# Convert the words in lower case and remove stop words from the stop_words curated list
+splitRDD_no_stop = splitRDD.filter(lambda x: x.lower() not in stop_words)
+
+# Create a tuple of the word and 1 
+splitRDD_no_stop_words = splitRDD_no_stop.map(lambda w: (w, 1))
+
+# Count of the number of occurences of each word
+resultRDD = splitRDD_no_stop_words.reduceByKey(lambda x, y: x + y)
+
+
+# Display the first 10 words and their frequencies from the input RDD
+for word in resultRDD.take(10):
+	print(word)
+
+# Swap the keys and values from the input RDD
+resultRDD_swap = resultRDD.map(lambda x: (x[1], x[0]))
+
+# Sort the keys in descending order
+resultRDD_swap_sort = resultRDD_swap.sortByKey(ascending=False)
+
+# Show the top 10 most frequent words and their frequencies from the sorted RDD
+for word in resultRDD_swap_sort.take(10):
+	print("{},{}". format(word[1], word[0]))
